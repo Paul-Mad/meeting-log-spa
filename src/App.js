f4c1d0195eb57592d35247bd146d25774b67d695
Paul-Mad <paulin.mad@hotmail.com>
2021-03-13 02:44:27 +0000

    Routes implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 // Import React
 import React, { Component } from "react";
-
+import { Router } from "@reach/router";
 import Home from "./Home";
 import Navigation from "./Navigation";
 import Welcome from "./Welcome";
+import Login from "./Login";
+import Meetings from "./Meetings";
+import Register from "./Register";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: "Paulo",
+      user: null,
     };
   }
 
   render() {
     return (
       <div>
-        <Navigation />
+        <Navigation user={this.state.user} />
         {this.state.user && <Welcome user={this.state.user} />}
-        <Home user={this.state.user} />;
+        <Router>
+          <Home path="/" user={this.state.user} />
+          <Login path="/login" />
+          <Meetings path="/meetings" />
+          <Register path="/register" />
+        </Router>
       </div>
     );
   }
