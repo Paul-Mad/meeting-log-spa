e87ba18c12125a06d471ed1d01ec8b020f7eabfb
Paul-Mad <paulin.mad@hotmail.com>
2021-03-13 02:05:30 +0000

    Navigation component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Home";
+import Navigation from "./Navigation";
+import Welcome from "./Welcome";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: "Paulo",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        {this.state.user && <Welcome user={this.state.user} />}
+        <Home user={this.state.user} />;
+      </div>
+    );
+  }
 }
 
 export default App;
